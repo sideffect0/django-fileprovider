2d437ea8940753e1b03fe20fdeb5d05f3e7e10c8
sideffect0 <mail3renjith@gmail.com>
2016-12-07 21:33:45 +0530

    update license

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Renjith Thankachan',
     author_email='mail3renjith@gmail.com',
     url='https://bitbucket.org/renlinx007/django-file-provider',
-    download_url='https://bitbucket.org/renlinx007/django-fileprovider/get/0.1a.tar.gz',
+    download_url='https://bitbucket.org/renlinx007/django-fileprovider/get/0.1a1.tar.gz',
     long_description=open('README.md', 'r').read(),
     packages=[
         'fileprovider',
@@ -23,7 +23,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
