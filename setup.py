aeea24bb8cfc3406911723499fea62ac3c9e978e
sideffect0 <mail3renjith@gmail.com>
2017-01-16 15:36:52 +0530

    version 0.1a4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__  = '0.1a3'
+__version__  = '0.1a4'
 
 setup(
     name='django-fileprovider',
@@ -9,7 +9,7 @@ setup(
     author='Renjith Thankachan',
     author_email='mail3renjith@gmail.com',
     url='https://bitbucket.org/renlinx007/django-file-provider',
-    download_url='https://bitbucket.org/renlinx007/django-fileprovider/get/0.1a3.tar.gz',
+    download_url='https://bitbucket.org/renlinx007/django-fileprovider/get/0.1a4.tar.gz',
     long_description=open('README.md', 'r').read(),
     packages=[
         'fileprovider',
